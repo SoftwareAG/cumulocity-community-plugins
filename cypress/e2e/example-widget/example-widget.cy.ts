f1c6916c5469f476b140a7c9e092df2a9dfd2a7f
Tristan Bastian <tristan.bastian@softwareag.com>
2023-02-08 17:19:01 +0100

    test(cypress): initial cypress setup (#2)
    * test: initial cypress setup
    
    * test: fix build
    
    * ci: add cypress to pr pipeline
    
    * ci: use different container
    
    * ci: move build and ci into single job
    
    * ci: remove needs
    
    * ci: use correct build path
    
    * ci: use correct cypress config
    
    * ci: set record to false

---
+++ cypress/e2e/example-widget/example-widget.cy.ts
@@ -0,0 +1,30 @@
+describe('example-widget', () => {
+  beforeEach(() => {
+    cy.interceptLoginOptions();
+    cy.interceptCurrentTenant();
+    cy.interceptCurrentUser();
+
+    cy.intercept(
+      '/inventory/managedObjects?fragmentType=c8y_Dashboard!name!home-cockpit1&pageSize=1',
+      { fixture: 'widgets/example-widget/cockpit-dashboard.json' }
+    ).as('cockpitDashboardConfig');
+
+    cy.visit('/apps/sag-pkg-community-widgets/#/');
+    cy.wait('@cockpitDashboardConfig');
+  });
+
+  it('view component should be present', () => {
+    cy.get('c8y-community-example-widget-plugin-view').should('exist');
+    cy.get('c8y-community-example-widget-plugin-view p.text').contains('abc');
+  });
+
+  it('config component should be present', () => {
+    cy.get('c8y-dashboard-child .header-actions a[title="Settings"]').click();
+    cy.get('bs-dropdown-container button[title="Edit widget"]').click();
+    cy.get('c8y-community-example-widget-plugin-config textarea').then(
+      ($textarea) => {
+        expect($textarea.val()).to.contain('abc');
+      }
+    );
+  });
+});
---
+++ cypress/support/commands.ts
@@ -0,0 +1,85 @@
+export {};
+
+declare global {
+  // eslint-disable-next-line @typescript-eslint/no-namespace
+  namespace Cypress {
+    interface Chainable {
+      interceptCurrentUser(customRoles?: string[]): Chainable<void>;
+      interceptCurrentTenant(): Chainable<void>;
+      interceptLoginOptions(): Chainable<void>;
+    }
+  }
+}
+
+Cypress.Commands.add('interceptCurrentUser', (customRoles?: string[]) => {
+  const defaultRoles = [
+    'ROLE_TENANT_ADMIN',
+    'ROLE_TENANT_MANAGEMENT_ADMIN',
+    'ROLE_TENANT_MANAGEMENT_READ',
+    'ROLE_ALARM_ADMIN',
+    'ROLE_ALARM_READ',
+    'ROLE_AUDIT_ADMIN',
+    'ROLE_AUDIT_READ',
+    'ROLE_DEVICE_CONTROL_ADMIN',
+    'ROLE_DEVICE_CONTROL_READ',
+    'ROLE_EVENT_ADMIN',
+    'ROLE_EVENT_READ',
+    'ROLE_IDENTITY_ADMIN',
+    'ROLE_IDENTITY_READ',
+    'ROLE_INVENTORY_ADMIN',
+    'ROLE_INVENTORY_READ',
+    'ROLE_INVENTORY_CREATE',
+    'ROLE_MEASUREMENT_ADMIN',
+    'ROLE_MEASUREMENT_READ',
+    'ROLE_APPLICATION_MANAGEMENT_ADMIN',
+    'ROLE_APPLICATION_MANAGEMENT_READ',
+    'ROLE_OPTION_MANAGEMENT_ADMIN',
+    'ROLE_OPTION_MANAGEMENT_READ',
+  ];
+  const roles = customRoles || defaultRoles;
+  const roleReferences = roles.map((roleId) => ({
+    role: { name: roleId, id: roleId },
+  }));
+  const user = {
+    roles: {
+      references: roleReferences,
+    },
+    passwordStrength: 'GREEN',
+    displayName: 'Max',
+    enabled: true,
+    devicePermissions: {},
+    supportUserEnabled: false,
+    id: 'Max',
+    email: 'max@example.com',
+    shouldResetPassword: false,
+    userName: 'Max',
+    customProperties: { userOrigin: 'BASIC' },
+    phone: '',
+    applications: [],
+  };
+  cy.intercept('/user/currentUser', (req) => {
+    req.reply(user);
+  }).as('currentUser');
+});
+
+Cypress.Commands.add('interceptCurrentTenant', () => {
+  cy.intercept('/tenant/currentTenant', (req) => {
+    req.reply({
+      allowCreateTenants: true,
+      customProperties: { gainsightEnabled: false },
+      domainName: 'test.example.com',
+      name: 'test',
+      applications: {
+        references: [],
+      },
+    });
+  }).as('currentTenant');
+});
+
+Cypress.Commands.add('interceptLoginOptions', () => {
+  cy.intercept('/tenant/loginOptions', (req) => {
+    req.reply({
+      loginOptions: [],
+    });
+  }).as('loginOptions');
+});
