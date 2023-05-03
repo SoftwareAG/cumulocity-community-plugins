8f53af3ed7742520590b14d49ec3faa01e4366b8
jdre <jdre@softwareag.com>
2023-05-03 11:45:20 +0200

    fix(datapoints-graph): realtime button styling fix
    realtime button style change to make pulse visible
    
    n/a

--- src/datapoints-graph/time-controls/realtime-control/realtime-control.component.ts
+++ src/datapoints-graph/time-controls/realtime-control/realtime-control.component.ts
@@ -4,6 +4,7 @@ import { gettext } from '@c8y/ngx-components';
 @Component({
   selector: 'c8y-realtime-control',
   templateUrl: './realtime-control.component.html',
+  styleUrls: ['./realtime-control.less'],
 })
 export class RealtimeControlComponent {
   @Input() active: boolean;
