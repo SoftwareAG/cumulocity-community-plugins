25b1dcb4e038abeddffb3153a5648c557d2830db
jdre <jdre@softwareag.com>
2024-04-24 13:55:24 +0200

    feat(datapoints-graph): alarm selector list item component
    Alarm selector list item color pick and display label
    
    n/a

--- src/datapoints-graph/alarm-selector/alarm-selector-list-item/alarm-selector-list-item.component.ts
+++ src/datapoints-graph/alarm-selector/alarm-selector-list-item/alarm-selector-list-item.component.ts
@@ -1,5 +1,23 @@
-import { Component, forwardRef } from '@angular/core';
-import { FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
+import {
+  Component,
+  EventEmitter,
+  forwardRef,
+  Input,
+  Output,
+} from '@angular/core';
+import {
+  AbstractControl,
+  ControlValueAccessor,
+  FormBuilder,
+  FormGroup,
+  NG_VALIDATORS,
+  NG_VALUE_ACCESSOR,
+  ValidationErrors,
+  Validator,
+} from '@angular/forms';
+import { Observable } from 'rxjs/internal/Observable';
+import { AlarmDetails } from '../alarm-selector-modal/alarm-selector-modal.model';
+import { map, startWith, take } from 'rxjs/operators';
 
 @Component({
   selector: 'c8y-alarm-selector-list-item',
@@ -17,6 +35,80 @@ import { FormGroup, NG_VALIDATORS, NG_VALUE_ACCESSOR } from '@angular/forms';
     },
   ],
 })
-export class AlarmSelectorListItemComponent {
+export class AlarmSelectorListItemComponent
+  implements ControlValueAccessor, Validator
+{
+  @Input() highlightText: string;
+  @Input() showAddRemoveButton = true;
+  @Input() isSelected = false;
+  @Input() optionToRemove = false;
+  @Input() showActiveToggle = false;
+  @Input() isCollapsed = true;
+
+  @Output() added = new EventEmitter<AlarmDetails>();
+  @Output() removed = new EventEmitter<AlarmDetails>();
+
   formGroup: FormGroup;
+  isValid$: Observable<boolean>;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.formGroup = this.formBuilder.group({
+      details: [],
+      color: [],
+      __active: [],
+      __target: [],
+      // fragment: [],
+      // series: [],
+      // __template: []
+    });
+    this.isValid$ = this.formGroup.statusChanges.pipe(
+      map((status) => status === 'VALID'),
+      startWith(this.formGroup.valid)
+    );
+  }
+
+  validate(_control: AbstractControl): ValidationErrors {
+    return this.formGroup?.valid ? null : { formInvalid: {} };
+  }
+
+  writeValue(obj: any): void {
+    this.formGroup.patchValue({ ...obj, details: obj });
+  }
+
+  registerOnChange(fn: any): void {
+    this.formGroup.valueChanges
+      .pipe(map((tmp) => this.transformFormValue(tmp)))
+      .subscribe(fn);
+  }
+
+  registerOnTouched(fn: any): void {
+    this.formGroup.valueChanges.pipe(take(1)).subscribe(fn);
+  }
+
+  setDisabledState(isDisabled: boolean): void {
+    isDisabled ? this.formGroup.disable() : this.formGroup.enable();
+  }
+
+  collapse() {
+    this.isCollapsed = !this.isCollapsed;
+  }
+
+  addOrRemoveItem() {
+    const value = this.transformFormValue(this.formGroup.value);
+    if (this.isSelected) {
+      this.removed.emit(value);
+    } else {
+      this.added.emit(value);
+    }
+  }
+
+  remove() {
+    this.removed.emit(this.transformFormValue(this.formGroup.value));
+  }
+
+  private transformFormValue(formValue: any) {
+    const obj = Object.assign({}, formValue.details || {}, formValue);
+    delete obj.details;
+    return obj;
+  }
 }
--- src/datapoints-graph/alarm-selector/alarm-selector.module.ts
+++ src/datapoints-graph/alarm-selector/alarm-selector.module.ts
@@ -3,9 +3,11 @@ import { DragDropModule } from '@angular/cdk/drag-drop';
 import { AlarmSelectionListComponent } from './alarm-selection-list/alarm-selection-list.component';
 import { CoreModule } from '@c8y/ngx-components';
 import { AlarmSelectorListItemComponent } from './alarm-selector-list-item/alarm-selector-list-item.component';
+import { PopoverModule } from 'ngx-bootstrap/popover';
+import { TooltipModule } from 'ngx-bootstrap/tooltip';
 
 @NgModule({
-  imports: [CoreModule, DragDropModule],
+  imports: [CoreModule, DragDropModule, PopoverModule, TooltipModule],
   declarations: [AlarmSelectionListComponent, AlarmSelectorListItemComponent],
   exports: [AlarmSelectionListComponent],
   providers: [],
