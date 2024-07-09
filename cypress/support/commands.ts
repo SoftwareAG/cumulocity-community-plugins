1494e9cb1d0a285a1a6372f6b31ed488cc8d5264
jdre <jdre@softwareag.com>
2024-07-09 10:29:54 +0200

    feat(datapoints-graph): datapoints graph refactor
    Duplicated code as cypress command
    
    n/a

--- cypress/support/commands.ts
+++ cypress/support/commands.ts
@@ -22,6 +22,7 @@ declare global {
       ): Chainable<void>;
       login2(username: string, password: string): Chainable<any>;
       getTenantId2(username: string, password: string): Chainable<string>;
+      prepareGroupWithDashboard(): Chainable<void>;
     }
   }
 }
@@ -199,5 +200,78 @@ Cypress.Commands.overwrite('request', (originalFn, ...args) => {
       [options.url, options.method, options.body] = args;
     }
     return originalFn(Object.assign({}, defaults, options));
+  });
+});
+
+Cypress.Commands.add('prepareGroupWithDashboard', () => {
+  cy.request({
+    url: '/inventory/managedObjects',
+    method: 'POST',
+    headers: { Accept: 'application/json' },
+    body: {
+      c8y_IsDeviceGroup: {},
+      c8y_Notes: '',
+      name: 'e2eCopyGroup',
+      type: 'c8y_DeviceGroup',
+    },
+  }).then((groupRes) => {
+    cy.request({
+      url: `/inventory/managedObjects/${groupRes.body.id}/childAdditions`,
+      method: 'POST',
+      headers: {
+        'Content-Type':
+          'application/vnd.com.nsn.cumulocity.managedobject+json;',
+        Accept: 'application/json',
+      },
+      body: {
+        name: 'e2eDashboard',
+        [`c8y_Dashboard!group!${groupRes.body.id}`]: {},
+        c8y_Dashboard: {
+          name: 'e2eDashboard',
+          priority: 10000,
+          icon: 'th',
+          translateWidgetTitle: true,
+          children: {
+            '1': {
+              componentId: 'datapoints-graph',
+              classes: {
+                'alerts-overlay': false,
+                'card-dashboard': true,
+                'panel-title-regular': true,
+                map: true,
+                card: true,
+              },
+              _x: 0,
+              _y: 0,
+              id: '1',
+              title: 'Data points graph',
+              _width: 12,
+              config: {
+                datapoints: [],
+                displayDateSelection: false,
+                displayAggregationSelection: false,
+                widgetInstanceGlobalTimeContext: false,
+                canDecoupleGlobalTimeContext: false,
+                dateFrom: '2023-04-27T12:00:00.000Z',
+                dateTo: '2023-04-27T12:10:00.000Z',
+                interval: 'hours',
+                aggregation: null,
+                realtime: true,
+                yAxisSplitLines: false,
+                xAxisSplitLines: false,
+              },
+              _height: 6,
+            },
+          },
+          classes: { 'dashboard-theme-light': true },
+          c8y_IsNavigatorNode: null,
+          widgetClasses: { 'panel-title-regular': true },
+        },
+      },
+    }).then((dashboardRes) => {
+      cy.visit(
+        `/apps/cockpit/index.html?remotes=%7B"sag-pkg-community-plugins"%3A%5B"ExampleWidgetPluginModule"%2C"DatapointsGraphWidgetModule"%5D%7D#/group/${groupRes.body.id}/dashboard/${dashboardRes.body.id}`
+      );
+    });
   });
 });
